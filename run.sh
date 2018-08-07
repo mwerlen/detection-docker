067b5028c502b600deb5e0ef43434de663759aa3
mwerlen <maxime@werlen.fr>
2018-08-07 16:44:38 +0200

    Adding options to enable debug window

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ docker run \
     --security-opt apparmor:unconfined \
     -it \
     --rm \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
     --volume /home/mwerlen/projects/detection-docker/visiona:/usr/local/src/visiona \
     --name detection mwerlen/detection:latest \
     /bin/bash
